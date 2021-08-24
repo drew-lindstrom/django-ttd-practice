0f90baae39ee623516f6c6fcb1cb459973da0c62
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-08-24 12:30:49 -0500

    Fixed error with calling home page view.

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from lists import views as lists_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(lists.urls))
+    path('', lists_views.home_page, name="home")
 ]
